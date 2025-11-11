87e7a705341328419e67dcd25b8b9163383a4e55
alihamza-12 <alihamzaarshad12@gmail.com>
2025-11-12 00:29:55 +0500

    chnages in the server file and the package

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
-import { createApp } from "json-server/lib/app.js";
-import { Low } from "lowdb";
-import { JSONFile } from "lowdb/node";
-import { Observer } from "json-server/lib/observer.js";
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
 
-const adapter = new JSONFile("db.json");
-const observer = new Observer(adapter);
-const db = new Low(observer, {});
-await db.read();
-
-const app = createApp(db, { static: [] });
+server.use(middlewares);
+server.use(router);
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, "0.0.0.0", () => {
+server.listen(port, "0.0.0.0", () => {
   console.log("JSON Server is running on port " + port);
 });
