1255e72f94353acb6308f33b9aea2e46b7016139
alihamza-12 <alihamzaarshad12@gmail.com>
2025-11-11 22:09:38 +0500

    doing

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
+const port = process.env.PORT || 3000;
+
 server.use(middlewares);
 server.use(router);
-
-const port = process.env.PORT || 3000;
 server.listen(port, () => {
-  console.log(`✅ JSON Server running on port ${port}`);
+  console.log("JSON Server is running on port " + port);
 });
